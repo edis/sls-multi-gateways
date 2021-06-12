6447a642e56330b019cb785e4e722dcbb4f4a9fd
Edis Gonuler <edis@gonuler.com>
2021-06-12 08:30:28 -0400

    add sample project for medium article

---
+++ sample/posts/handler.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports.helloWorld = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Welcome to the posts service!',
+    }),
+  };
+
+  callback(null, response);
+};
